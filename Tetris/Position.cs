8fa1c58e8a5b8d961e866a0bc538de4e601092f3
Moeseu <kostkuzmenko@gmail.com>
2024-01-24 10:38:59 +0200

    Оберти, пересування І-блок
    Оберти, пересування І-блок

--- Tetris/Position.cs
+++ Tetris/Position.cs
@@ -2,6 +2,13 @@
 {
     public class Position
     {
+        public int Row {  get; set; }
+        public int Column {  get; set; }
 
+        public Position(int row, int column)
+        {
+            Row = row; 
+            Column = column;
+        }
     }
 }
