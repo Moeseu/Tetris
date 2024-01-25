96fb03cf06b9b06362594d10e3fb5b9a1bd72bdd
Moeseu <kostkuzmenko@gmail.com>
2024-01-25 12:08:37 +0200

    Черга

---
+++ Tetris/BlockQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new OBlock(),
+            new IBlock(),
+            new L_Block(),
+            new JBlock(),
+            new S_Block(),
+            new Z_Block(),
+            new T_Block()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock{  get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while(block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
